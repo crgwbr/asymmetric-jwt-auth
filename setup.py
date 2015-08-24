cc77192c236be2cbb71bb1111e0e9445ce06160c
Craig Weber <crgwbr@gmail.com>
2015-08-24 14:25:16 -0400

    Include migrations in build

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ packages = [
     'asymmetric_jwt_auth.test',
     'asymmetric_jwt_auth.management',
     'asymmetric_jwt_auth.management.commands',
+    'asymmetric_jwt_auth.migrations',
 ]
 
 requires = [
@@ -15,7 +16,7 @@ requires = [
 
 setup(
     name='asymmetric_jwt_auth',
-    version='0.1.5',
+    version='0.1.6',
     description='Asymmetric key based authentication for HTTP APIs',
     long_description=open('README.rst').read(),
     author='Craig Weber',
