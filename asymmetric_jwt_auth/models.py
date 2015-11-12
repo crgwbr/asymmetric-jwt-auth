b34d009b4360c99f7ee4b94fd794550ce936f951
Craig Weber <crgwbr@gmail.com>
2015-11-12 10:34:35 -0500

    Fixes #1 Change key reference from User to settings.AUTH_USER_MODEL

--- asymmetric_jwt_auth/models.py
+++ asymmetric_jwt_auth/models.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class PublicKey(models.Model):
-    user = models.ForeignKey(User, related_name='public_keys')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='public_keys')
     key = models.TextField(help_text="The user's RSA public key")
