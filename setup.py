eb63331e501868a9733e9ee2a6018b313d5fe394
Craig Weber <crgwbr@gmail.com>
2015-11-12 11:06:22 -0500

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='asymmetric_jwt_auth',
-    version='0.1.7',
+    version='0.2.0',
     description='Asymmetric key based authentication for HTTP APIs',
     long_description=open('README.rst').read(),
     author='Craig Weber',
