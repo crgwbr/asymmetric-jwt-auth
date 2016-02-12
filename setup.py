d604cf497fe181a215bcab0199d4e7237217cf23
Craig Weber <crgwbr@gmail.com>
2016-02-12 16:28:08 -0500

    Fix README codec issue

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import codecs
+import os.path
 from distutils.core import setup
 
 packages = [
@@ -14,11 +16,17 @@ requires = [
     'cryptography>=1.0',
 ]
 
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
 setup(
     name='asymmetric_jwt_auth',
-    version='0.2.1',
+    version='0.2.2',
     description='Asymmetric key based authentication for HTTP APIs',
-    long_description=open('README.rst').read(),
+    long_description=read(fpath('README.rst')),
     author='Craig Weber',
     author_email='crgwbr@gmail.com',
     url='https://github.com/crgwbr/asymmetric_jwt_auth',
