93faccfcb12f5a2aaad8143c6e397b3dea511099
Craig Weber <crgwbr@gmail.com>
2018-04-05 11:08:25 -0400

    In Django Admin, make user a raw ID field for performance

--- src/asymmetric_jwt_auth/admin.py
+++ src/asymmetric_jwt_auth/admin.py
@@ -5,3 +5,5 @@ from . import models
 @admin.register(models.PublicKey)
 class PublicKeyAdmin(admin.ModelAdmin):
     list_display = ['user', 'comment']
+    fields = ['user', 'comment', 'key']
+    raw_id_fields = ['user']
